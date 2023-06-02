0e8ac60416b1b482af227259a7186c272278a750
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-06-02 22:55:36 +0300

    add icon

--- src/components/Pages/TweetsPage/TweetsPage.jsx
+++ src/components/Pages/TweetsPage/TweetsPage.jsx
@@ -1,31 +1,51 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import UserCard from 'components/modules/UserCard/UserCard';
 import Btn from 'shared/Button/Button';
 
 const TweetsPage = () => {
-  const users = [
+  const [users, setUsers] = useState([
     {
       user: 'Elon Mask',
       tweets: 777,
       followers: 100500,
       avatar: '../../../shared/images/Hansel.jpg',
       id: 1,
+      isFollowing: false,
     },
-    // {
-    //   user: 'Keanu Reeves',
-    //   tweets: 555,
-    //   followers: 800300,
-    //   avatar: 'url12.jpg',
-    //   id: 2,
-    // },
-    // {
-    //   user: 'Joe Biden',
-    //   tweets: 333,
-    //   followers: 1000001,
-    //   avatar: 'url13.jpg',
-    //   id: 3,
-    // },
-  ];
+    {
+      user: 'Keanu Reeves',
+      tweets: 555,
+      followers: 800300,
+      avatar: 'url12.jpg',
+      id: 2,
+      isFollowing: false,
+    },
+    {
+      user: 'Joe Biden',
+      tweets: 333,
+      followers: 1000001,
+      avatar: 'url13.jpg',
+      id: 3,
+      isFollowing: false,
+    },
+  ]);
+
+  const handleFollow = userId => {
+    setUsers(prevUsers =>
+      prevUsers.map(user =>
+        user.id === userId
+          ? {
+              ...user,
+              isFollowing: !user.isFollowing,
+              followers: user.isFollowing
+                ? user.followers + 1
+                : user.followers - 1,
+            }
+          : user
+      )
+    );
+  };
 
   return (
     <>
@@ -37,8 +57,8 @@ const TweetsPage = () => {
             key={user.id}
             avatar={user.avatar}
             followers={user.followers}
-            // isFollowing={isFollowing}
-            // handleFollow={handleFollow}
+            isFollowing={user.isFollowing}
+            handleFollow={() => handleFollow(user.id)}
           />
         ))}
       </div>
--- src/components/modules/UserCard/UserCard.jsx
+++ src/components/modules/UserCard/UserCard.jsx
@@ -1,5 +1,7 @@
 import styles from './UserCard.module.scss';
 import Btn from '../../../shared/Button/Button';
+import LogoIcon from 'shared/icons/LogoIcon';
+import Avatar from 'shared/icons/Avatar';
 
 const UserCard = ({
   user,
@@ -12,30 +14,29 @@ const UserCard = ({
   return (
     <>
       <div className={styles.tweetCard}>
-        <div className={styles.svgContainer}></div>
+        <div className={styles.svgContainer}>
+          <span className={styles.Logo}>
+            <LogoIcon />
+          </span>
+        </div>
         <div className={styles.divider}>
           <div className={styles.avatar}>
-            {/* <svg className={styles.avatar_user}>
-              <use href="../../../shared/images/avatar.svg"></use>
-            </svg> */}
-            <img
-              className={styles.avatar_user}
-              src={avatar}
-              width="80"
-              alt="User Avatar"
-            />
+            <span className={styles.avatar_user}>
+              <Avatar />
+            </span>
           </div>
         </div>
 
         <div className={styles.infoBlockWrapper}>
           <div className={styles.infoBlock}>
             <p className={styles.text}>{tweets} TWEETS</p>
-
             <p className={styles.text}>{followers} FOLLOWERS</p>
-
-            <Btn onClick={handleFollow}>
-              {isFollowing ? 'Following' : 'Follow'}
-            </Btn>
+            <div>
+              <Btn onClick={handleFollow}>
+                {isFollowing ? 'Following' : 'Follow'}
+              </Btn>
+              <span>{followers.toLocaleString()}</span>
+            </div>
           </div>
         </div>
       </div>
