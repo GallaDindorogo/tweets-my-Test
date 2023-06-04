3d6410f0e85526e8a610d7ad0ec2fe5e53f2ca72
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-06-04 08:16:14 +0300

    fix routers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Layout from './modules/Layout/Layout';
 import HomePage from './Pages/HomePage/HomePage';
@@ -9,9 +9,9 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/tweets" element={<TweetsPage />} />
-        <Route path="*" element={<HomePage />} />
+        <Route path="tweets" element={<TweetsPage />} />
       </Route>
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 };
