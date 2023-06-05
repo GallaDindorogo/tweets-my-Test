7b7f685a442fca97b193055df2ff2d02e8a9e994
Galla Dindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-06-05 22:13:24 +0300

    Update HomePage.jsx

--- src/components/Pages/HomePage/HomePage.jsx
+++ src/components/Pages/HomePage/HomePage.jsx
@@ -3,7 +3,13 @@ import styles from './HomePage.module.scss';
 const HomePage = () => {
   return (
     <>
-      <div className={styles.homeBody}></div>
+      <div className={styles.homeBodyWrapper}>
+        <div className={styles.homeBody}></div>
+        <p className={styles.text}>
+          Discover the joy of tracking tweets and managing followers in this
+          intuitive project. Let's get started!
+        </p>
+      </div>
     </>
   );
 };
